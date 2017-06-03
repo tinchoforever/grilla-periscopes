8a261f7f84df5a6e4ea3e4f2fbaf2838de353d85
tinchoforever <tinchoforever@gmail.com>
2017-06-03 20:45:31 -0300

    goal

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 
+  var forceCssLink = document.createElement("link");
+  forceCssLink.href = "https://vivo-anfibio.herokuapp.com/styles/force-periscope.css"; 
+  forceCssLink.rel = "stylesheet"; 
+  forceCssLink.type = "text/css"; 
+
+  var stylesCssLink = document.createElement("link");
+  stylesCssLink.href = "https://vivo-anfibio.herokuapp.com/styles/main.css"; 
+  stylesCssLink.rel = "stylesheet"; 
+  stylesCssLink.type = "text/css"; 
+
+  setTimeout(function(){
+    $('iframe').each(function(d) { 
+      this.document.body.appendChild(stylesCss);
+      this.document.body.appendChild(stylesCssLink);
+    });  
+  },10000)
+  
+  
 
 });
