f01de5b319ebda48972fd1f8cc04cefbe2976ed2
tinchoforever <tinchoforever@gmail.com>
2017-06-03 20:57:59 -0300

    hacker

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
   var forceCssLink = document.createElement("link");
-  forceCssLink.href = "https://vivo-anfibio.herokuapp.com/styles/force-periscope.css"; 
+  forceCssLink.href = "https://vivo-anfibio.herokuapp.com/css/force-periscope.css"; 
   forceCssLink.rel = "stylesheet"; 
   forceCssLink.type = "text/css"; 
 
   var stylesCssLink = document.createElement("link");
-  stylesCssLink.href = "https://vivo-anfibio.herokuapp.com/styles/main.css"; 
+  stylesCssLink.href = "https://vivo-anfibio.herokuapp.com/css/main.css"; 
   stylesCssLink.rel = "stylesheet"; 
   stylesCssLink.type = "text/css"; 
 
   setTimeout(function(){
     $('iframe').each(function(d) { 
-      this.document.body.appendChild(stylesCss);
-      this.document.body.appendChild(stylesCssLink);
+     $(this).contents().find("head").append($("<style type='text/css'>.TweetAuthor,.timeline-Header.timeline-InformationCircle-widgetParent,.timeline-Tweet-brand.u-floatRight,.timeline-Tweet-metadata,footer.timeline-Footer.u-cf,h1.timeline-Header-title,p.timeline-Tweet-text,ul.timeline-Tweet-actions{display:none}.timeline-Body.customisable-border,.timeline-Viewport{margin:0;padding:0;border:0}</style>"));
+
     });  
-  },10000)
+  },1000);
   
   
 
